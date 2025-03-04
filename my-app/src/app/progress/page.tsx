e058d509940453c82e245246157ac47aa63edfc4
akshayasenthil28 <akshayasenthil00@gmail.com>
2025-03-04 16:57:53 +0530

    coded the react imports

---
+++ my-app/src/app/progress/page.tsx
@@ -0,0 +1,3 @@
+import React, { useState } from 'react';
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
