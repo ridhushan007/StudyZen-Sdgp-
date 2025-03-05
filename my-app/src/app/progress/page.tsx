005916b324ada9a460158d76599bead910040f4a
akshayasenthil28 <akshayasenthil00@gmail.com>
2025-03-05 23:24:52 +0530

    Added help button

--- my-app/src/app/progress/page.tsx
+++ my-app/src/app/progress/page.tsx
@@ -199,6 +199,16 @@ export default function StudyZenDashboard() {
                        </div>
                       </CardContent>
                     </Card>
+                    
+                    <div className="fixed bottom-4 right-4 flex items-center">
+                      <button className="flex items-center gap-2 bg-white border rounded-full px-4 py-2 shadow-md">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-600" viewBox="0 0 20 20" fill="currentColor">
+                          <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
+                        </svg>
+                        <span className="font-medium">Need Help?</span>
+                      </button>
+                    </div>
+                  </div>  
 
             
 
