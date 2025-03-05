4434b2dccaa8f011513790f75b239cb5dee91444
akshayasenthil28 <akshayasenthil00@gmail.com>
2025-03-05 21:38:28 +0530

    Added Public Confession section

--- my-app/src/app/progress/page.tsx
+++ my-app/src/app/progress/page.tsx
@@ -84,4 +84,11 @@ export default function StudyZenDashboard() {
                 <path fillRule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clipRule="evenodd" />
               </svg>
             </div>
+            <div className="flex items-center p-3 text-gray-200 rounded hover:bg-slate-600">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-3" viewBox="0 0 20 20" fill="currentColor">
+                <path d="M2 5a2 2 0 012-2h7a2 2 0 012 2v4a2 2 0 01-2 2H9l-3 3v-3H4a2 2 0 01-2-2V5z" />
+                <path d="M15 7v2a4 4 0 01-4 4H9.828l-1.766 1.767c.28.149.599.233.938.233h2l3 3v-3h2a2 2 0 002-2V9a2 2 0 00-2-2h-1z" />
+              </svg>
+            </div>
             
+
