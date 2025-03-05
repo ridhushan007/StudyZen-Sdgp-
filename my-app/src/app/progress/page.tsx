707b78b09a1cc419c08265c45982b2a8982aee41
akshayasenthil28 <akshayasenthil00@gmail.com>
2025-03-05 21:36:49 +0530

    Added quiz creation option to sidebar

--- my-app/src/app/progress/page.tsx
+++ my-app/src/app/progress/page.tsx
@@ -79,3 +79,9 @@ export default function StudyZenDashboard() {
                 <path d="M3 8a2 2 0 012-2v10h8a2 2 0 01-2 2H5a2 2 0 01-2-2V8z" />
               </svg>
             </div>
+            <div className="flex items-center p-3 text-gray-200 rounded hover:bg-slate-600">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-3" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clipRule="evenodd" />
+              </svg>
+            </div>
+            
