adf462da1a1efdda3601008db6d259b2bd3a3093
Mushaf Ahmed <mushafahmed12@gmail.com>
2025-03-18 14:45:11 +0530

    updated next.config.js

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,30 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com",
-          port: "",
-          pathname: "/**",
-        },
-      ],
-    },
-    async headers() {
-      return [
-        {
-          source: "/:path*",
-          headers: [
-            {
-              key: "Content-Security-Policy",
-              value: "frame-src 'self' https://open.spotify.com;",
-            },
-          ],
-        },
-      ]
-    },
-  }
-  
-  module.exports = nextConfig
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-src 'self' https://open.spotify.com;",
+          },
+        ],
+      },
+    ]
+  },
+  // Add API routes redirect
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3001/api/:path*',
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
   
   
