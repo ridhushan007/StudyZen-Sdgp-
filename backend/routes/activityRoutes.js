550b360747f968fd086671c7851202ae068775c0
NatashaMarris <natashamarriz16@gmail.com>
2025-03-22 00:23:41 +0530

    Define POST route for adding a new activity

--- backend/routes/activityRoutes.js
+++ backend/routes/activityRoutes.js
@@ -3,3 +3,4 @@ const router = express.Router();
 const activityController = require('../controllers/activityController');
 
 router.get('/recent/:userId', activityController.getRecentActivities);
+router.post('/add', activityController.addActivity);
