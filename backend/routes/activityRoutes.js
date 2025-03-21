64c664c7371bf31630d67b0dba824ffbfc02ce09
NatashaMarris <natashamarriz16@gmail.com>
2025-03-22 00:24:35 +0530

    Define GET route for fetching last active time

--- backend/routes/activityRoutes.js
+++ backend/routes/activityRoutes.js
@@ -4,3 +4,4 @@ const activityController = require('../controllers/activityController');
 
 router.get('/recent/:userId', activityController.getRecentActivities);
 router.post('/add', activityController.addActivity);
+router.get('/last-active/:userId', activityController.getLastActiveTime);
