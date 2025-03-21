ee20e8e82fb3943bf9045894a082b79485621cd1
manulyaa <manulyamj@gmail.com>
2025-03-21 16:03:54 +0530

    add dashboard page with user authentication and profile display

---
+++ my-app/src/app/Login-page-loader/page.tsx
@@ -0,0 +1,5 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { FaUser, FaSignOutAlt, FaBook, FaCalendarAlt, FaClipboardList } from 'react-icons/fa'
