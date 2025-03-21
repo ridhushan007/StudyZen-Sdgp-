887b70c45baf41db336439afff93a194bd1de337
NatashaMarris <natashamarriz16@gmail.com>
2025-03-22 00:22:40 +0530

    Define GET route for fetching recent activities

--- backend/routes/activityRoutes.js
+++ backend/routes/activityRoutes.js
@@ -1,3 +1,5 @@
 const express = require('express');
 const router = express.Router();
 const activityController = require('../controllers/activityController');
+
+router.get('/recent/:userId', activityController.getRecentActivities);
