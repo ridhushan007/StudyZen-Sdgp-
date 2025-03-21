e7bbb9c45839c00e0263adff1409c69a106c686a
NatashaMarris <natashamarriz16@gmail.com>
2025-03-22 00:26:05 +0530

    Updated the whole activityRoutes.js

--- backend/routes/activityRoutes.js
+++ backend/routes/activityRoutes.js
@@ -5,3 +5,6 @@ const activityController = require('../controllers/activityController');
 router.get('/recent/:userId', activityController.getRecentActivities);
 router.post('/add', activityController.addActivity);
 router.get('/last-active/:userId', activityController.getLastActiveTime);
+
+module.exports = router;
+
