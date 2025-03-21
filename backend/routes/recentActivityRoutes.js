52b59f30452051da385d99397ee04b564f251b36
NatashaMarris <natashamarriz16@gmail.com>
2025-03-22 00:37:23 +0530

    Ensure MongoDB client connects only if not already connected

--- backend/routes/recentActivityRoutes.js
+++ backend/routes/recentActivityRoutes.js
@@ -7,5 +7,12 @@ const client = new MongoClient(uri);
 let isConnected = false;
 
 async function getRecentActivities(req, res) {
+    try {
+        if (!isConnected) {
+          await client.connect();
+          isConnected = true;
+        }
+    
+        const db = client.db('test');
   
   
