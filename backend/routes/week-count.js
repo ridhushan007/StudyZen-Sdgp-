a48012a243d34416d5b77a12cba6b6effb912252
NatashaMarris <natashamarriz16@gmail.com>
2025-03-22 01:19:31 +0530

    Connected to MongoDB using environment variables

--- backend/routes/week-count.js
+++ backend/routes/week-count.js
@@ -2,3 +2,8 @@ const express = require('express');
 const Quiz = require('../models/Quiz');
 const router = express.Router();
 const { MongoClient } = require('mongodb');
+
+const uri = process.env.MONGO_URI;
+  const client = new MongoClient(uri);
+  
+  let isConnected = false;
